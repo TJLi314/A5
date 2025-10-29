394bbcf3a1da85b9e011aff17baf36a36d6398bc
TJ Li <TianjianLi314@gmail.com>
2025-10-29 10:00:24 -0500

    run and compile scripts

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+cd /storage-home/t/tl107/comp432/A5/Build
+echo 5 | ~/scons/bin/scons-3.1.2
---
+++ run.sh
@@ -0,0 +1,4 @@
+clear
+cd /storage-home/t/tl107/comp432/A5/Build
+echo 5 | ~/scons/bin/scons-3.1.2
+bin/sqlUnitTest myCat foo
